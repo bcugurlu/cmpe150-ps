d940a58856a1359c85b433148e7552613c12d7c1
ASUS <burakcan.ugurlu@gmail.com>
2022-11-06 15:47:12 +0300

    PS1 questions and answers are added.

--- CmpE150_PS1/Area of a circle.py
+++
@@ -1,7 +0,0 @@
-def area(r):
-    A = pi*r**2
-    print(A)
-
-pi = 3.14
-radius = int(input())
-area(radius)
--- CmpE150_PS1/Body-mass index.py
+++
@@ -1,6 +0,0 @@
-def bmi(w, h):
-    return w/(2.54*h/100)**2
-
-weight = float(input())
-height = float(input())
-print("%.2f" % bmi(weight, height))
